d463c5241b0dbb539e7e54faa4b97efe31fc34f6
Lucas Correia <lucascorreia-.-@live.com>
2018-11-14 19:00:15 -0200

    Inicio do P2

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy;2018 
+            <a href="https://www.linkedin.com/in/lucas-correia-086619b6/"
+                target="_blank"> Lucas Correia</a>.
+        </strong>
+    </footer>
+)
